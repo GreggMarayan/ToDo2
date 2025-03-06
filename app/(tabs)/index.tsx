20b83a5575be3a6b958f0216a0646c8347c30b86
Gregg <gregg.marayan@gmail.com>
2025-03-06 15:30:47 +0800

    Added Input and fixed some design issues

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,20 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { TextInput } from 'react-native-gesture-handler'
 
 const app = () => {
   return (
-
+<SafeAreaProvider>
     <SafeAreaView style={Style.page}>
       <Image source={require('../../assets/images/react-logo.png')} style={Style.img} />
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={Style.text}>Login!</Text>
+      <View>
+        <TextInput style={Style.input} placeholder='Username'/>
+        <TextInput style={Style.input} placeholder="Password"/>
       </View>
     </SafeAreaView>
-  )
+</SafeAreaProvider>
+)
 }
 
 export default app
@@ -32,6 +35,19 @@ const Style = StyleSheet.create({
 
   img: {
     width: 100,
-    height: 100}
+    height: 100
+  },
+
+  input: {
+    borderWidth: 2,
+    borderColor: 'white',
+    backgroundColor: 'grey',
+    color: 'white',
+    width: 200,
+    height: 20, 
+    borderRadius: 10,
+    margin: 10,
+    padding: 5
+    }
 
 })
